e3902ec3b6c1daf921e519972c7485357788f3ba
BibicoBolado <rodrigosiqueira@id.uff.br>
2018-06-18 21:34:19 -0300

    first commit]

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['ponto-turistico.herokuapp.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['','127.0.0.1', 'localhost']
 
 
 # Application definition
